fe625dd7f0ec4ca454102cfc76d4322dede057f0
Gustavo Anjos <gustavoanjos42@gmail.com>
2020-09-22 14:42:09 -0400

    fix: update user class and create swift file enums

---
+++ entrepreneurship-great-app/Enums.swift
@@ -0,0 +1,23 @@
+//
+//  Enums.swift
+//  entrepreneurship-great-app
+//
+//  Created by Gustavo Anjos on 22/09/20.
+//
+
+import Foundation
+
+enum typesBusiness {
+    case digital
+    case fisico
+    case ambos
+}
+
+enum socialNetworks {
+    case instagram
+    case facebook
+    case twitter
+    case linkedin
+    case tiktok
+    case whatsapp
+}
--- entrepreneurship-great-app/User.swift
+++ entrepreneurship-great-app/User.swift
@@ -6,24 +6,39 @@
 //
 
 import Foundation
+import UIKit
 
 class User {
     var name: String
     var email: String
     var password: String
     var location: String?
+    var typeBuniness: typesBusiness
+    var areasExpertise: [String]?
+    var socialNetworks: [socialNetworks]?
+    var picture: UIImage?
     
-    init(name: String, email: String, password: String, location: String? = nil) {
+    init(name: String, email: String, password: String, location: String? = nil, typeBunisses: typesBusiness, areasExpertise: [String] = [], socialNetworks: [socialNetworks] = []) {
         self.name = name
         self.email = name
         self.password = password
         self.location = location
+        self.typeBuniness = typeBunisses
+        self.areasExpertise = areasExpertise
+        self.socialNetworks = socialNetworks
+        self.picture = UIImage(named: self.name)
     }
     
 }
+ 
 
 extension User{
     static let all = [
-        User(name: "Misericordia Store", email: "misericordiastore@apple.com", password: "aaaa", location: "")
+        User(name: "Misericordia Store", email: "misericordiastore@apple.com", password: "aaaa", typeBunisses: .digital, areasExpertise: ["Design"], socialNetworks: [.whatsapp, .facebook, .twitter]),
+        User(name: "Os Barés", email: "bares@apple.com", password: "bbbb", typeBunisses: .fisico, areasExpertise: ["Artesanato"], socialNetworks: [.whatsapp, .instagram]),
+        User(name: "Manaos", email: "manaos@apple.com", password: "cccc", location: "Brasil", typeBunisses: .ambos, areasExpertise: ["Gráfica"], socialNetworks: [.linkedin])
     ]
 }
+
+
+
