bdced1228102c2d27b7c3d3c33490d007003afda
Vinicius Silva Alencar <vinicius.s.alencar@outlook.com>
2020-09-30 09:58:28 -0400

    feat: create a login view controller

---
+++ entrepreneurship-great-app/controllers/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  entrepreneurship-great-app
+//
+//  Created by Vinicius Alencar on 30/09/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInWithAppleButton(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
