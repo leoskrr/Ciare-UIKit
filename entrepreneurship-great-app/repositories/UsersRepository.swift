953e257adfda096512ad5fe6b0fa08633b10faa3
Leonardo Viana <sonleo2013@gmail.com>
2020-10-01 11:12:24 -0400

    feat: sign in and sign up user

--- entrepreneurship-great-app/repositories/UsersRepository.swift
+++ entrepreneurship-great-app/repositories/UsersRepository.swift
@@ -18,38 +18,46 @@ class UsersRepository{
     }
     
     public func saveUser(userId: String, name: String, email: String){
-        let record = CKRecord(recordType: "User", recordID: CKRecord.ID(recordName: userId))
+        let record = CKRecord(recordType: "Users", recordID: CKRecord.ID(recordName: userId))
         
         record["name"] = name
         record["email"] = email
         
-        publicDatabase.save(record) {_,_ in
-            UserDefaults.standard.setValue(record.recordID.recordName, forKey: "userProfileId")
+        publicDatabase.save(record) { record, err in
+//            UserDefaults.standard.setValue(record.recordID.recordName, forKey: "userProfileId")
+            if let user = record {
+                print("\n\n User successfully saved \n\n")
+                print(user)
+            }
+            if let error = err {
+                print("\n\n Error saving user \n\n")
+                print(error)
+            }
             //criar retorno de objeto da classe User se der tudo certo
         }
         //retornar nil se der errado
     }
     
-    public func fetchUser(userId: String) -> User?{
+    public func fetchUser(userId: String){
         publicDatabase.fetch(withRecordID: CKRecord.ID(recordName: userId)) { (record, error) in
             if let fetchedInfo = record {
-                
-                let name = fetchedInfo["name"] as? String
+                print(fetchedInfo)
+//                let name = fetchedInfo["name"] as? String
 //                let email = fetchedInfo["email"] as? String
 //                let expertiseAreas = fetchedInfo["expertiseAreas"] as? [String]
 //                let location = fetchedInfo["location"] as? CLLocation?
 //                let socialNetworks = fetchedInfo["socialNetworks"] as? [String]
 //                let typeBusiness = fetchedInfo["typeBusiness"] as? String
 //                let availablePartnerships = fetchedInfo["availablePartnerships"] as? Int
-                
-//                let fetchedUser = User(id: userId, name: name, email: email, location: location, typeBusiness: typeBusiness, areasExpertise: expertiseAreas, socialNetworks: socialNetworks, picture: nil, availablePartnerships: Bool(availablePartnerships as NSNumber), partners: nil, conversations: nil, blocked: nil, followers: nil, following: nil)
 
                 //You can now use the user name and email address (like save it to local)
-                UserDefaults.standard.set(name, forKey: "userName")
-                UserDefaults.standard.set(userId, forKey: "userProfileID")
+//                UserDefaults.standard.set(name, forKey: "userName")
+//                UserDefaults.standard.set(userId, forKey: "userProfileID")
+            }
+            if let handleError = error {
+                print(handleError)
             }
         }
-        return nil
     }
     
     public func findUserById(_ id: String) -> User? {
